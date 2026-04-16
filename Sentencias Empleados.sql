7c67fa07c6cbe6e0658cd66d3a745b08525604bf
Bi-For-Dummies <sergiorus9608@gmail.com>
2026-04-16 08:47:53 -0500

    Create Sentencias Empleados.sql

---
+++ Sentencias Empleados.sql
@@ -0,0 +1,95 @@
+--Crear tabla en esquema
+
+CREATE TABLE IF NOT EXISTS gth_asistencia.empleados_app (
+    cedula text NOT NULL,
+    nombre text,
+    contrato bigint,
+    cod_cc text,
+    centro_de_costos text,
+    cod_cargo numeric,
+    cargo text,
+    retirado text,
+    email text,
+    telefono_1 text,
+    porcentaje_arp text,
+    ultima_sincronizacion timestamp with time zone DEFAULT now(),
+    CONSTRAINT empleados_app_pkey PRIMARY KEY (cedula)
+);
+
+--Insertar datos en tabla
+
+INSERT INTO gth_asistencia.empleados_app (
+    cedula, 
+    nombre, 
+    contrato, 
+    cod_cc, 
+    centro_de_costos, 
+    cod_cargo, 
+    cargo, 
+    retirado,
+    email,
+    telefono_1,
+)
+SELECT DISTINCT ON (cedula) 
+    CASE 
+        WHEN cedula LIKE '%E%' THEN split_part((cedula::numeric)::text, '.', 1)
+        ELSE cedula
+    END as cedula, 
+    nombre, 
+    contrato, 
+    cod_cc, 
+    centro_de_costos, 
+    cod_cargo, 
+    cargo, 
+    retirado,
+    email, 
+    telefono_1,
+FROM public.dim_historico_empleados
+ORDER BY cedula, contrato DESC
+ON CONFLICT (cedula) DO UPDATE SET
+    nombre = EXCLUDED.nombre,
+    contrato = EXCLUDED.contrato,
+    cod_cc = EXCLUDED.cod_cc,
+    centro_de_costos = EXCLUDED.centro_de_costos,
+    cargo = EXCLUDED.cargo,
+    retirado = EXCLUDED.retirado,
+    email = EXCLUDED.email,
+    telefono_1 = EXCLUDED.telefono_1,
+    ultima_sincronizacion = now();
+
+--Automatizar actualización
+
+CREATE OR REPLACE FUNCTION gth_asistencia.fn_sync_personal_a_app()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO gth_asistencia.empleados_app (
+        cedula, nombre, contrato, cod_cc, centro_de_costos, cod_cargo, cargo, retirado, email, telefono_1, porcentaje_arp
+    )
+    VALUES (
+        CASE 
+            WHEN NEW.cedula LIKE '%E%' THEN split_part((NEW.cedula::numeric)::text, '.', 1)
+            ELSE NEW.cedula
+        END,
+        NEW.nombre, NEW.contrato, NEW.cod_cc, NEW.centro_de_costos, NEW.cod_cargo, NEW.cargo, NEW.retirado, NEW.email, NEW.telefono_1, NEW.porcentaje_arp
+    )
+    ON CONFLICT (cedula) DO UPDATE SET
+        nombre = EXCLUDED.nombre,
+        contrato = EXCLUDED.contrato,
+        cod_cc = EXCLUDED.cod_cc,
+        centro_de_costos = EXCLUDED.centro_de_costos,
+        cargo = EXCLUDED.cargo,
+        retirado = EXCLUDED.retirado,
+        email = EXCLUDED.email,
+        telefono_1 = EXCLUDED.telefono_1,
+        porcentaje_arp = EXCLUDED.porcentaje_arp,
+        ultima_sincronizacion = now();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+--Trigger
+
+CREATE TRIGGER trg_sync_personal_gth_asistencia
+AFTER INSERT OR UPDATE ON public.dim_historico_empleados
+FOR EACH ROW
+EXECUTE FUNCTION gth_asistencia.fn_sync_personal_a_app();
